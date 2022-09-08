dd3c7f05747885866a89997c7d44b47e88a4aca3
Pilar Sauthier <pilarsauthier@Pilars-MacBook-Pro.local>
2022-09-08 19:32:15 -0300

    home

---
+++ proyecto.prog/src/Components/Home/Home.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+
+class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+    componentDidMount(){
+        fetch
+    }
+
+
+
+}
+
+export default Home;
